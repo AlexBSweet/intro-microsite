45d66233d4eb877386b77cea710892d2b34b2fe3
Alex Sweet <alexsweet@Alexs-MacBook-Pro-2.local>
2019-09-11 12:02:13 -0500

    serious progress

--- main.js
+++ main.js
@@ -1,9 +1,39 @@
-const startButton = document.querySelector('#start-button')
+const counterOne = document.querySelector('input[name="counter-one"]')
+const counterTwo = document.querySelector('input[name="counter-two"]')
+const counterThree = document.querySelector('input[name="counter-three"]')
 
+    anime({
+        targets: '.counter-one',
+        value: [0, 1000],
+        round: 1,
+        duration: 3000,
+        easing: 'easeInOutExpo', 
+        direction: 'normal', 
+        loop: 'false',
+        autoplay: 'false',
+        delay: 2000
+      });
 
+      anime({
+        targets: '.counter-two',
+        value: [0, 22],
+        round: 1,
+        duration: 3000,
+        easing: 'easeInOutExpo', 
+        direction: 'normal', 
+        loop: 'false',
+        autoplay: 'false',
+        delay: 2000
+      });
 
-
-startButton.addEventListener('click', function(){
-    var c = new CountUp('styles',0,100)
-    c.start()
-})
+      anime({
+        targets: '.counter-three',
+        value: [0, 48],
+        round: 1,
+        duration: 3000,
+        easing: 'easeInOutExpo', 
+        direction: 'normal', 
+        loop: 'false',
+        autoplay: 'false',
+        delay: 2000
+      });
