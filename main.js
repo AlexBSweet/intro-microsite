d00f6668bb2502f6fb6ff0e0157f966ad019804f
Alex Sweet <alexsweet@Alexs-MacBook-Pro-2.local>
2019-09-11 22:07:33 -0500

    more progress

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+
+
+// COUNTING ANIIMATION SECTION
+
 const counterOne = document.querySelector('input[name="counter-one"]')
 const counterTwo = document.querySelector('input[name="counter-two"]')
 const counterThree = document.querySelector('input[name="counter-three"]')
@@ -37,3 +41,129 @@ const counterThree = document.querySelector('input[name="counter-three"]')
         autoplay: 'false',
         delay: 2000
       });
+
+
+      // CHARACTER VIEWER SECTION
+      
+      const characterViewSection = document.querySelector('section.character-viewer')
+      const characterContainer = document.querySelector('div.character-container')
+      const gridContainer = document.querySelector('section.grid-container')
+      const characters = gridContainer.querySelectorAll('div')
+      const characterCursor = document.querySelector('div.character-cursor')
+
+      characterViewSection.addEventListener('mouseenter', ()=>{
+        characterCursor.style.visibility = 'visible'
+        characterContainer.style.visibility = 'visible'
+      })
+
+      characterViewSection.addEventListener('mouseleave', ()=>{
+        characterCursor.style.visibility = 'hidden'
+        characterContainer.style.visibility = 'hidden'
+      })
+
+      characterViewSection.addEventListener('mousemove', (e)=>{
+          const X = e.pageX
+          const Y = e.pageY
+          
+          characterCursor.style.left = X + 'px'
+          characterCursor.style.top = Y + 'px'
+          characterContainer.style.left = X + 'px'
+          characterContainer.style.top = Y + 'px'
+      })
+
+      
+
+      characters.forEach(div=>{
+          const character = div.innerHTML
+
+          div.addEventListener('mouseenter', function (){
+            
+            
+            characterContainer.innerHTML = character
+          })
+      })
+      
+ 
+
+    // DRAGGABLE SECTION
+
+    const draggableDiv = document.querySelector('div.draggable')
+    const draggableSection = document.querySelector('section.draggable-section')
+
+    // draggableDiv.addEventListener('mousedown', (e)=>{
+    //   const X = e.pageX
+    //   const Y = e.pageY
+
+    //   const draggableTop = draggableSection.offsetTop
+    //   const topY = Y - draggableTop
+
+    //   draggableDiv.style.top = topY +'px'
+    //   draggableDiv.style.left = X + 'px'
+    //   console.log(X, topY)
+    // })
+
+
+    var dragItem = document.querySelector("#item");
+    var container = document.querySelector("#container");
+
+    var active = false;
+    var currentX;
+    var currentY;
+    var initialX;
+    var initialY;
+    var xOffset = 0;
+    var yOffset = 0;
+
+    container.addEventListener("touchstart", dragStart, false);
+    container.addEventListener("touchend", dragEnd, false);
+    container.addEventListener("touchmove", drag, false);
+
+    container.addEventListener("mousedown", dragStart, false);
+    container.addEventListener("mouseup", dragEnd, false);
+    container.addEventListener("mousemove", drag, false);
+
+    // clientX and clientY output the coordinates of the mouse when pressed
+    function dragStart(e) {
+      if (e.type === "touchstart") {
+        initialX = e.touches[0].clientX - xOffset;
+        initialY = e.touches[0].clientY - yOffset;
+      } else {
+        initialX = e.clientX - xOffset;
+        initialY = e.clientY - yOffset;
+      }
+
+      if (e.target === dragItem) {
+        active = true;
+      }
+    }
+
+    function dragEnd(e) {
+      initialX = currentX;
+      initialY = currentY;
+
+      active = false;
+    }
+
+    function drag(e) {
+      if (active) {
+      
+        e.preventDefault();
+      
+        if (e.type === "touchmove") {
+          currentX = e.touches[0].clientX - initialX;
+          currentY = e.touches[0].clientY - initialY;
+        } else {
+          currentX = e.clientX - initialX;
+          currentY = e.clientY - initialY;
+        }
+
+        xOffset = currentX;
+        yOffset = currentY;
+
+        setTranslate(currentX, currentY, dragItem);
+      }
+    }
+
+    function setTranslate(xPos, yPos, el) {
+      el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+    }
--- type-tester.js
+++ type-tester.js
@@ -1,19 +1,35 @@
-
-const leadingTag = document.querySelector('input[name="leading"]')
+const typeTester = document.querySelector('section.type-tester')
+const lineHeightTag = document.querySelector('input[name="leading"]')
 const typeSize = document.querySelector('input[name="typesize"]')
 const slantTag = document.querySelector('input[name="slant"]')
 const weightTag = document.querySelector('input[name="weight"]')
+const spacingTag = document.querySelector('input[name="tracking"]')
 const outputTag = document.querySelector('div.type-output')
-const trackingTag = document.querySelector('input[name="tracking"]')
+
+const typeSizeOutput = document.querySelector('.typesize-output')
+const weightOutput = document.querySelector('.weight-output')
+const slantOutput = document.querySelector('.slant-output')
+const lineHeightOutput = document.querySelector('.line-height-output')
+const spacingOutput = document.querySelector('.spacing-output')
 
 
-trackingTag.addEventListener('input', function(){
-    outputTag.style.letterSpacing = this.value + 'em'
-   
+// this resets the innerHTML on outputTag to be empty on first click only if the type says "Try Intro..."
+outputTag.addEventListener('click', ()=>{
+    
+    if(outputTag.innerHTML.indexOf("Try Intro...") !== -1) {
+        outputTag.innerHTML = ''
+    }
 })
 
-leadingTag.addEventListener('input', function(){
+
+spacingTag.addEventListener('input', function(){
+    outputTag.style.letterSpacing = this.value + 'em'
+    spacingOutput.innerHTML = this.value
+})
+
+lineHeightTag.addEventListener('input', function(){
     outputTag.style.lineHeight = this.value
+    lineHeightOutput.innerHTML = this.value
 })
 
 
@@ -21,6 +37,7 @@ slantTag.addEventListener('input', function(){
     const weight = weightTag.value
     const slant = slantTag.value
     outputTag.style['font-variation-settings'] = `"wght" ${weight}, "slnt" ${slant}`
+    slantOutput.innerHTML = this.value
 })
 
 
@@ -29,6 +46,7 @@ weightTag.addEventListener('input', function(){
     const weight = weightTag.value
     const slant = slantTag.value
     outputTag.style['font-variation-settings'] = `"wght" ${weight}, "slnt" ${slant}`
+    weightOutput.innerHTML = this.value
 })
 
 
@@ -36,4 +54,86 @@ weightTag.addEventListener('input', function(){
 
 typeSize.addEventListener('input', function(){
         outputTag.style.fontSize = this.value + 'px'
+        typeSizeOutput.innerHTML = this.value + 'px'
 })
+
+
+
+const parLeft = document.querySelector('p.paragraph-left')
+const parCenter = document.querySelector('p.paragraph-center')
+const parRight = document.querySelector('p.paragraph-right')
+
+parLeft.addEventListener('click', ()=>{
+    outputTag.style.textAlign = 'left'
+})
+parCenter.addEventListener('click', ()=>{
+    outputTag.style.textAlign = 'center'
+})
+parRight.addEventListener('click', ()=>{
+    outputTag.style.textAlign = 'right'
+})
+
+const ipsumTag = document.querySelector('.ipsum')
+
+ipsumTag.addEventListener('click', ()=>{
+    outputTag.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+
+    outputTag.style.fontSize = 60 + 'px'
+    typeSize.value = 60
+    typeSizeOutput.innerHTML = 60 + 'px'
+    outputTag.style.lineHeight = 1.5
+    lineHeightOutput.innerHTML = 1.5
+    lineHeightTag.value = 1.5
+    
+})
+
+
+const colorCircles = document.querySelector('.color-circles')
+const blackCircle = document.querySelector('.black-circle')
+const redCircle = document.querySelector('.red-circle')
+const grayCircle = document.querySelector('.gray-circle')
+const greenCircle = document.querySelector('.green-circle')
+
+const rangeTags = document.querySelectorAll('input[type=range]')
+
+blackCircle.addEventListener('click', ()=>{
+    typeTester.style.backgroundColor = '#000'
+    typeTester.style.color = '#fff'
+    rangeTags.forEach(tag=>{
+        tag.style.background = "#fff"
+    })
+})
+redCircle.addEventListener('click', ()=>{
+    typeTester.style.backgroundColor = '#fa5454'
+    typeTester.style.color = '#fff'
+    rangeTags.forEach(tag=>{
+        tag.style.background = "#424242"
+    })
+})
+grayCircle.addEventListener('click', ()=>{
+    typeTester.style.backgroundColor = '#dedede'
+    typeTester.style.color = '#000'
+    rangeTags.forEach(tag=>{
+        tag.style.background = "#424242"
+    })
+})
+greenCircle.addEventListener('click', ()=>{
+    typeTester.style.backgroundColor = '#0d3622'
+    typeTester.style.color = '#fff'
+    rangeTags.forEach(tag=>{
+        tag.style.background = "#fff"
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
